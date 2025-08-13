1415504cb6264f58784232ddd02bc329b6381cfd
Antonio <aspans@gmail.com>
2025-08-13 13:19:48 +0200

    feat(app-tienda-online): tienda online finished

---
+++ app-tienda-online-practica/src/app/product-list/product-item/product-item.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item',
+  imports: [],
+  templateUrl: './product-item.html',
+  styleUrl: './product-item.css'
+})
+export class ProductItem {
+  item = {
+    name: "",
+    price: 100,
+    symbol: "€",
+  }
+}
