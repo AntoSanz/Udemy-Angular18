585bd95788985403a8e745814a5ee50045c82339
Antonio <aspans@gmail.com>
2025-08-13 12:21:28 +0200

    chore(app-interpolation): change names and interpolation component

--- app-components-angular/src/app/app.ts
+++ app-components-angular/src/app/app.ts
@@ -1,16 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppFirstComponent } from "./app-first-component/app-first-component";
-import { AppComponentInline } from "./app-component-inline/app-component-inline";
+import { AppFirstComponent } from "./first-component/first-component";
+import { AppComponentInline } from "./component-inline/component-inline";
+import { Interpolacion } from "./interpolacion/interpolacion";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppFirstComponent, AppComponentInline],
+  imports: [RouterOutlet, AppFirstComponent, AppComponentInline, Interpolacion],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
-export class App {
-  
-}
+export class App {}
