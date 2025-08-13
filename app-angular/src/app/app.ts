43e2616c1c102177ac4ef0efd9b2f4dac258cd36
Antonio <aspans@gmail.com>
2025-08-13 14:47:10 +0200

    feat(app-angular)_: two way data binding

--- app-angular/src/app/app.ts
+++ app-angular/src/app/app.ts
@@ -3,14 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './child-component/child-component';
 import { ShowMessage } from './show-message/show-message';
 import { ReflectText } from './reflect-text/reflect-text';
+import { TwoWayBinding } from "./two-way-binding/two-way-binding";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChildComponent, ShowMessage, ReflectText],
+  imports: [RouterOutlet, ChildComponent, ShowMessage, ReflectText, TwoWayBinding],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   title: string = 'Hola Angular';
+  eventBindingTitle: string = 'Event Binding'
   propertyBindingTitle: string = 'Property Binding';
+  twoWayTitle: string = 'Two Way Binding'
 }
